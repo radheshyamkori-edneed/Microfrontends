53716ecd4dcfff1700e8a2f72dd27fa80515535a
Radheshyam Kori <radheshyam.kori@edneed.com>
2022-05-19 20:02:18 +0530

    Container workflow configuration and adding Header for CSS

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 //import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 //console.log(mount);
 export default () => {
   return (
+    <BrowserRouter>
       <div>
           <h1>Hi there!</h1>
           <hr/>
+          <Header />
           <MarketingApp />
       </div>
-  );  
-  
+    </BrowserRouter>      
+  );    
 };
 
